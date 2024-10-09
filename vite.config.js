fef1cbac50c93ea69daee9bcf8a72427e944912d
Oliver <oliver@MSI>
2024-10-08 22:26:10 -0700

    added env for vite proxy

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+
+// Get the backend URL from environment variables
+const backendUrl = process.env.VITE_BACKEND_URL || 'http://localhost:3000';
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
       '/api': { 
-        target: 'http://localhost:3000', // Backend server URL
+        target: backendUrl, // Backend server URL
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, '') 
       }
